6779480d1a59e8a1a38398a279f898924a70d3b0
Dmitry Mass <dimamikron41@gmail.com>
2021-12-18 16:49:50 +0300

    complete

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,13 @@
+let text = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae beatae fugiat voluptatem, expedita hic, aut porro minus possimus quidem corrupti mollitia quos quaerat, perferendis ea aperiam blanditiis ipsam laudantium et.`;
 function capitalize(string) {
+  text = string.split(" ");
+
+  let result = text.map(function (item) {
+    return item[0].toUpperCase() + item.slice(1);
+  });
+  return result.join(" ");
 }
+let endResult = capitalize(text);
+console.log(endResult);
 
 module.exports = capitalize;
